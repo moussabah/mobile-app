4e2ff34764f0bf8350ac444b8a186253998e5707
Tuo Adama <adama.tuo@etudiant.univ-rennes1.fr>
2024-03-30 09:57:05 +0100

    feat: comment TODO for unimplement service

--- services/FilterService.ts
+++ services/FilterService.ts
@@ -5,12 +5,14 @@ import {config} from "../config/config";
 export default class FilterService {
 
     static filterByCriteria(filter: Criteria): any[]{
+        //TODO
         const url:string = config.domain+"/criteria"
         const result = HttpRequestService.getData(url, {});
         return [];
     }
 
     static filterBySearch(data: any[], search: string){
+        //TODO
         const url:string = config.domain+"/search"
         const result = HttpRequestService.getData(url, {});
         return [];
--- services/HttpRequestService.ts
+++ services/HttpRequestService.ts
@@ -1,10 +1,12 @@
 export default class HttpRequestService {
 
     static async getData(url:string, body: {}): any[]{
+        //TODO
         return [];
     }
 
     static async  postData(url:string, body:{}): any[]{
+        //TODO
         return [];
     }
 }
