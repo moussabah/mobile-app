42b875aa7c6b9f92bcd3459058c426934f61fc7f
Tuo Adama <adama.tuo@etudiant.univ-rennes1.fr>
2024-04-01 16:12:33 +0200

    feat: user event item initialise

--- screens/UserEventScreen.jsx
+++ screens/UserEventScreen.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
-import {Text} from "react-native";
-
+import {Button, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Styles} from "../assets/styles/Styles";
+import {Colors} from "../assets/styles/Colors";
 function UserEventScreen(props) {
+
+    function EventItem(){
+        return (
+        <View style={componentScreen.eventItem}>
+            <View style={componentScreen.itemImage}>
+                <Text>Image</Text>
+            </View>
+            <View style={componentScreen.itemDescription}>
+                <Text>Description</Text>
+            </View>
+        </View>
+        )
+    }
+
+
     return (
-        <Text>User Event screen</Text>
+        <View style={componentScreen.container}>
+            <TouchableOpacity title="Ajouter un évenement" style={componentScreen.addBtn}>
+                <Text style={componentScreen.btnText}>Ajouter un événement</Text>
+            </TouchableOpacity>
+            <ScrollView>
+                <EventItem />
+            </ScrollView>
+        </View>
     );
 }
+
+
+const componentScreen = StyleSheet.create({
+    container: {
+        paddingHorizontal: 10,
+        paddingTop: 10,
+    },
+    eventItem: {
+        flexDirection:"row"
+    },
+    itemImage:{
+        flex: 1,
+        borderWidth: 1,
+    },
+    itemDescription:{
+        flex: 2,
+        borderWidth: 1,
+    },
+    addBtn:{
+        backgroundColor: Colors.primary,
+        justifyContent: "center",
+        alignItems:"center",
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 10,
+    },
+    btnText: {
+        color: "white",
+        fontSize: 18,
+    }
+})
 
 export default UserEventScreen;
