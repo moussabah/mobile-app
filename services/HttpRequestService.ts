ca5382f4cc3511a9a66b296d6af38212faf88689
Tuo Adama <adama.tuo@etudiant.univ-rennes1.fr>
2024-04-05 19:30:13 +0200

    fix: method return type

--- services/HttpRequestService.ts
+++ services/HttpRequestService.ts
@@ -1,11 +1,11 @@
 export default class HttpRequestService {
 
-    static async getData(url:string, body: {}): any[]{
+    static async getData(url:string, body: {}): Promise<any[]>{
         //TODO
         return [];
     }
 
-    static async  postData(url:string, body:{}): any[]{
+    static async  postData(url:string, body:{}): Promise<any[]>{
         //TODO
         return [];
     }
