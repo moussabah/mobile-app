89d2ec93862fc802a88ac63f16573353fbafb9e6
mbah <moussa.bah@etudiant.univ-rennes1.fr>
2024-04-06 10:14:34 +0200

    Ajout des parcours

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Form from './screens/Form';
 import { NavigationContainer } from '@react-navigation/native';
 import EventScreen from "./screens/EventScreen";
 import EventDetail from "./screens/EventDetail";
+import CourseManagementScreen from "./parcours-events/CourseManagementScreen";
+import CreateCourseScreen from "./parcours-events/CreateCourseScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -12,9 +14,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          <Stack.Screen name="EventScreen" component={EventScreen} options={{title: 'Liste des événements'}} />
+          <Stack.Screen name="EventScreen" component={EventScreen} options={{title: 'Liste des evenements'}} />
           <Stack.Screen name="Form" component={Form} />
         <Stack.Screen name="EventDetail" component={EventDetail} options={{title: 'Description'}} />
+        <Stack.Screen name="CouseManagementScreen" component={CouseManagementScreen}
+                        Options={{title: 'CouseManagementScreen'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
