e9e262b5066adaaf414b976e441e2d468bbce127
Tuo Adama <adama.tuo@etudiant.univ-rennes1.fr>
2024-04-11 23:57:21 +0200

    feat: interactive card

---
+++ screens/MapViewScreen.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {StyleSheet, View} from "react-native";
+import MapView, {Marker} from "react-native-maps";
+
+function MapViewScreen(props) {
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                latitude: 49.163128,
+                longitude: -0.34709599999999996,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+                }}>
+                <Marker
+                    key={1}
+                    coordinate={{
+                        latitude: 49.163128,
+                        longitude: -0.34709599999999996,
+                    }}
+                    title={"Festival"}
+                    description={"2022-23"}
+                />
+                <Marker
+                    key={2}
+                    coordinate={{
+                        latitude: 46.163128,
+                        longitude: -1.34709599999999996,
+                    }}
+                    title={"Festival"}
+                    description={"2022-23"}
+                />
+                <Marker
+                    key={3}
+                    coordinate={{
+                        latitude: 49.163128,
+                        longitude: -0.34709599999999996,
+                    }}
+                    title={"RDV Diapason"}
+                    description={"2023-24"}
+                />
+                <Marker
+                    key={4}
+                    coordinate={{
+                        latitude: 48.163128,
+                        longitude: -0.34709599999999996,
+                    }}
+                    title={"Concert Fally Ipupa"}
+                    description={"2022-23"}
+                />
+            </MapView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: "100%",
+        height: "100%"
+    }
+})
+
+export default MapViewScreen;
